75eda970f0c122237b9c1fcbdadfb26d95e314f3
KuyTat <kuytat64@gmail.com>
2026-05-18 14:45:46 +0700

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -13,4 +13,38 @@ export class App {
   age: number = 19;
   profile: string = 'image.jpg';
   profile2: string = 'image2.jpg';
+
+  txt_color: string = 'blue';
+  txt_size: string = '40';
+
+  ChangeColor(): void {
+    if (this.txt_color == 'blue') {
+      this.txt_color = 'red';
+    } else {
+      this.txt_color = 'blue';
+    }
+  }
+  IncreaseFontSize(): void {
+    let fn: number = parseInt(this.txt_size);
+    this.txt_size = (fn + 10).toString();
+  }
+  DecreaseFontSize(): void {
+    let fn: number = parseInt(this.txt_size);
+    this.txt_size = (fn - 10).toString();
+  }
+
+  productImage: string[] = [
+    '/products/p1.webp',
+    '/products/p2.webp',
+    '/products/p3.webp',
+    '/products/p4.png',
+  ];
+  t_yellow: string = 'yellow';
+  btn_T(): void {
+    this.t_yellow = 'blue';
+  }
+  p: string[] = ['h1', 'h2', 'h3'];
+  alert(): void {
+    alert('hello');
+  }
 }
